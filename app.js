8a26571512fdf0e64935dfcac13ff12f4875e955
Varun Ramesh <varunramesh@live.com>
2015-04-23 20:08:45 -0700

    Store items in cache, keyed by commit sha.

--- app.js
+++ app.js
@@ -22,17 +22,27 @@ var db = levelup('./cache');
  * @param proceed - a function(err, data), which proceeds with the data
  */
 function check_cache(key, generate_data, proceed) {
-  db.get(key, function (err, value) {
-    if (err) {
+  // Check for the Key in the database
+  db.get(key, function(err, value) {
+    if (err) { // Key not found
+      // Call the data generation callback
       generate_data(function(err, data) {
-        if(err) {
+        if (err) { // Error generating data
           proceed(err);
         } else {
-          db.put(key, data);
+          // Save generated data to DB
+          db.put(key, JSON.stringify(data), function(err) {
+            if (err) {
+              console.error("Could not persist " + key + " to cache.");
+            } else {
+              console.log("Wrote " + key + " to cache.");
+            }
+          });
           proceed(null, data);
         }
       });
-    } else {
+    } else { // Key found, pass on the value
+      console.log("Read " + key + " from cache.");
       proceed(null, JSON.parse(value));
     }
   });
@@ -60,8 +70,8 @@ app.use(express.static('public'));
 function get_context(code, line) {
   var lines = code.split(/\r*\n/);
   var CONTEXT = 4;
-  var start = _.max([0, line - CONTEXT])
-  var end = _.min([lines.length - 1, line + CONTEXT])
+  var start = _.max([0, line - CONTEXT]);
+  var end = _.min([lines.length - 1, line + CONTEXT]);
   return lines.slice(start, end).join('\n');
 }
 
@@ -71,13 +81,14 @@ function render_list(user, repo, items, request, response) {
   response.render('repo.html', {
     user: user,
     repo: repo,
-    items_by_type:  items_by_type,
+    items_by_type: items_by_type,
     items_by_file: items_by_file
   });
 }
 
 // Route that access the tip of the default branch
 app.get('/:user/:repo', function(request, response) {
+  // Get the info for this github repo
   github.repos.get({
     user: request.params.user,
     repo: request.params.repo
@@ -85,6 +96,7 @@ app.get('/:user/:repo', function(request, response) {
     if (err) {
       response.send(err);
     } else {
+      // Get information on the default branch
       var default_branch = result.default_branch;
       github.repos.getBranch({
         user: request.params.user,
@@ -95,49 +107,56 @@ app.get('/:user/:repo', function(request, response) {
           response.send(err);
         } else {
           var sha = result.commit.sha;
-          github.gitdata.getTree({
-            user: request.params.user,
-            repo: request.params.repo,
-            sha: sha,
-            recursive: true
-          }, function(err, result) {
-            if (err) {
-              response.send(err);
-            } else {
-              async.mapSeries(result.tree, function(file, callback) {
-                var url = "https://raw.githubusercontent.com/" +
-                  request.params.user + "/" + request.params.repo +
-                  "/" + sha + "/" + file.path;
+          // Check to see if the current commit is in the cache
+          check_cache(sha, function(commit_items_callback) {
+            github.gitdata.getTree({
+              user: request.params.user,
+              repo: request.params.repo,
+              sha: sha,
+              recursive: true
+            }, function(err, result) {
+              if (err) {
+                commit_items_callback(err);
+              } else {
+                async.mapSeries(result.tree, function(file, callback) {
+                  var url = "https://raw.githubusercontent.com/" +
+                    request.params.user + "/" + request.params.repo +
+                    "/" + sha + "/" + file.path;
 
-                _request(url, function(error, response, body) {
-                  if (error) {
-                    callback(error);
+                  _request(url, function(error, response, body) {
+                    if (error) {
+                      callback(error);
+                    } else {
+                      callback(null, {
+                        content: body,
+                        path: file.path
+                      });
+                    }
+                  });
+                }, function(err, results) {
+                  if (err) {
+                    commit_items_callback(err);
                   } else {
-                    callback(null, {
-                      content: body,
-                      path: file.path
+                    var items = [];
+                    _.each(results, function(file) {
+                      var marks = todolist.findMarks(file.content);
+                      _.each(marks, function(mark) {
+                        mark.file = file.path;
+                        mark.context = get_context(file.content, mark.line);
+                        items.push(mark);
+                      });
                     });
+                    commit_items_callback(null, items);
                   }
                 });
-              }, function(err, results) {
-                if (err) {
-                  response.send(err);
-                } else {
-                  var items = [];
-                  _.each(results, function(file) {
-                    var marks = todolist.findMarks(file.content);
-                    _.each(marks, function(mark) {
-                      mark.file = file.path;
-                      mark.context = get_context(file.content, mark.line);
-                      items.push(mark);
-                    });
-                  });
-
-                  render_list(request.params.user, request.params.repo, items, request, response);
-                }
-              });
+              }
+            });
+          }, function(err, commit_items) {
+            if (err) {
+              response.send(err);
             }
-          });
+            render_list(request.params.user, request.params.repo, commit_items, request, response);
+          })
         }
       })
     }
